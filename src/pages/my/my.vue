c576e7c380841657b506252b3159bd6eb9832ae8
kokonally <1195218191@qq.com>
2023-11-30 19:03:31 +0800

    测试请求成功

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
-
+import '@/utils/http'
 const memberStore = useMemberStore()
+function getData() {
+  uni.request({
+    method: 'GET',
+    url: '/home/banner',
+  })
+}
 </script>
 
 <template>
@@ -20,6 +26,7 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getData" size="mini" plain type="primary">测试请求</button>
   </view>
 </template>
 
