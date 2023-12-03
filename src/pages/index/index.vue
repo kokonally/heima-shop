cf26823b0d5446a1d26b934657e738b0793896f0
kokonally <1195218191@qq.com>
2023-12-03 14:05:16 +0800

    猜你喜欢组件的封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -39,16 +39,26 @@ onLoad(() => {
 <template>
   <!--导航栏-->
   <CustomNavbar />
-  <!--轮播图-->
-  <XtxSwiper :list="bannerList" />
-  <!--分类-->
-  <CategoryPanel :list="categoryList" />
-  <!--热门推荐-->
-  <HotPanel :list="hotList" />
+  <scroll-view class="scroll-view" scroll-y>
+    <!--轮播图-->
+    <XtxSwiper :list="bannerList" />
+    <!--分类-->
+    <CategoryPanel :list="categoryList" />
+    <!--热门推荐-->
+    <HotPanel :list="hotList" />
+    <!--猜你喜欢-->
+    <XtxGuess />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
