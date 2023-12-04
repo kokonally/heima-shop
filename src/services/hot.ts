ee0ebc3d2897832501496020b4c89c9c00fced0c
kokonally <1195218191@qq.com>
2023-12-04 16:20:00 +0800

    热门推荐的类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,5 +1,6 @@
 import { http } from '@/utils/http'
-import type { PageParams } from '@/types/global'
+import type { PageParams, PageResult } from '@/types/global'
+import type { HotResult } from '@/types/hot'
 
 /**
  * 获取热门推荐
@@ -7,7 +8,7 @@ import type { PageParams } from '@/types/global'
  * @param data 请求参数
  */
 export const getHotRecommendAPI = (url: string, data?: PageParams & { subType?: string }) => {
-  return http({
+  return http<HotResult>({
     method: 'GET',
     url,
     data,
