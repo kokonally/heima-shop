feb4b2ac211430e39153bec6f7a325253954a0af
kokonally <1195218191@qq.com>
2023-12-04 22:38:34 +0800

    商品详情的弹出层交互

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -4,6 +4,8 @@ import { getGoodsByIdAPI } from '@/services/goods'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { GoodsResult } from '@/types/goods'
+import AddressPanel from '@/pages/goods/components/AddressPanel.vue'
+import ServicePanel from '@/pages/goods/components/ServicePanel.vue'
 
 const { safeAreaInsets } = uni.getSystemInfoSync()
 const pros = defineProps<{
@@ -42,6 +44,14 @@ const popup = ref<{
   ) => void
   close: () => void
 }>()
+
+//弹出层的名字
+const popupName = ref<'address' | 'service'>()
+//修改弹出层的名字
+const openPopup = (name: typeof popupName.value) => {
+  popupName.value = name
+  popup.value?.open()
+}
 </script>
 
 <template>
@@ -78,11 +88,11 @@ const popup = ref<{
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格</text>
         </view>
-        <view class="item arrow">
+        <view @tap="openPopup('address')" class="item arrow">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址</text>
         </view>
-        <view @tap="popup?.open()" class="item arrow">
+        <view @tap="openPopup('service')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮</text>
         </view>
@@ -160,9 +170,8 @@ const popup = ref<{
 
   <!--弹出层-->
   <uni-popup background-color="#fff" ref="popup" type="bottom">
-    <view>内容1</view>
-    <view>内容2</view>
-    <button @tap="popup?.close()">关闭</button>
+    <AddressPanel v-if="popupName === 'address'" @close="popup?.close()" />
+    <ServicePanel v-if="popupName === 'service'" @close="popup?.close()" />
   </uni-popup>
 </template>
 
