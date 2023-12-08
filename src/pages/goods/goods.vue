86578e8d8dd94db381ad8fe099d093d4fcba2ee4
kokonally <1195218191@qq.com>
2023-12-08 19:55:33 +0800

    购物车模块两个购物车

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -231,7 +231,7 @@ async function onAddCart(ev: SkuPopupEvent) {
         <text class="icon-handset"></text>
         客服
       </button>
-      <navigator class="icons-button" url="/pages/cart/cart" open-type="switchTab">
+      <navigator class="icons-button" url="/pages/cart/cart2" open-type="navigate">
         <text class="icon-cart"></text>
         购物车
       </navigator>
